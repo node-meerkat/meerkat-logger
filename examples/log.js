1765ccd21238047054046d8bba8cef4580a95d71
vardking <vardking@gmail.com>
2017-12-26 17:39:37 +0800

    to es6

--- examples/log.js
+++ examples/log.js
@@ -1,6 +1,8 @@
-var logger = require('pomelo-logger').getLogger('log', __filename, process.pid);
+'use strict';
+const logger = require('../index').getLogger('log', __filename, process.pid);
 
 process.env.LOGGER_LINE = true;
+logger.level = 'debug';
 logger.info('test1');
 logger.warn('test2');
 logger.error('test3');
